e7fda1268ec9f4b39974790001337dc56f9f3e0a
JFalat <joanna.falatowicz@gmail.com>
2022-05-09 22:58:25 +0200

    Homework 5 new packages

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from application import Application
+from fixture.application import Application
 import pytest
-from contact import Contact
+from model.contact import Contact
 
 @pytest.fixture
 def app(request):
@@ -13,16 +13,7 @@ def test_add_contact(app):
         app.login(username="admin", password="secret")
         app.contact_creation_and_submit(Contact(firstname= "Joanna", middlename="Test", lastname="LastTest", nickname="Test1", title="Test2", company="TestCompany", address="abc",
                                          mobile="123456789", home= "12345678", work="123456", fax="12345", email="a@a.com", email2="b@b.com", email3="c@c.com",
-                                         homepage="aaa",
-                                                bday="15",
-                                                bmonth="May",
-                                                byear="1970",
-                                                aday="21",
-                                                amonth="December",
-                                                ayear="1980",
-                                                address2="werewr",
-                                                phone2="123456",
-                                                notes="abc"))
+                                         homepage="aaa", bday="15", bmonth="May", byear="1970", aday="21", amonth="December", ayear="1980", address2="werewr", phone2="123456", notes="abc"))
         app.logout()
 
 
