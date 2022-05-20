06aa17e31f89e2796d83d4f6d8416be32f2ffdb2
JFalat <joanna.falatowicz@gmail.com>
2022-05-20 22:32:34 +0200

    Homework 8

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,3 +1,14 @@
-def test_modify_contact(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.modify_first_contact()
+from model.contact import Contact
+
+
+def test_modify_first_name(app):
+    app.contact.modify_first_contact(Contact(name="New contact"))
+
+def test_modify_middlename(app):
+    app.contact.modify_first_contact(Contact(middlename="Middle contact"))
+
+
+def test_modify_titlw(app):
+    app.contact.modify_first_contact(Contact(title="title"))
+
+
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,4 +1,9 @@
-def test_delete_first_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.modify_first_group()
-    app.session.logout()
+from model.group import Group
+
+def test_modify_first_name(app):
+    app.group.modify_first_group(Group(name="New group"))
+
+def test_modify_first_header(app):
+    app.group.modify_first_group(Group(header="New header"))
+
+
