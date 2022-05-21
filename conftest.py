7816d6c0211950c328240ee43bb34ca466b6f70a
JFalat <joanna.falatowicz@gmail.com>
2022-05-21 22:48:59 +0200

    Homework 8_2

--- conftest.py
+++ conftest.py
@@ -8,6 +8,7 @@ def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
+        fixture.session.login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
             fixture = Application()
@@ -15,7 +16,7 @@ def app(request):
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
-def app(request):
+def stop(request):
     def fin():
         fixture.session.logout()
         fixture.destroy()
