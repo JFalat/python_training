12b11c503b584135624ac5846e53f88f634a6851
JFalat <joanna.falatowicz@gmail.com>
2022-05-23 09:00:05 +0200

    Homework 9 fix

--- fixture/contact.py
+++ fixture/contact.py
@@ -80,6 +80,11 @@ class ContactHelper:
         # submit update
         wd.find_element_by_name("update").click()
 
+    def go_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
+
     def count(self):
         wd = self.app.wd
+        self.go_to_home_page()
         return len(wd.find_elements_by_name("selected[]"))
