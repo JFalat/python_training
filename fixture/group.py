2978dfa286367e99cec36e3b6bbd9db984e77aa3
JFalat <joanna.falatowicz@gmail.com>
2022-05-23 11:35:24 +0200

    Homework 10 question

--- fixture/group.py
+++ fixture/group.py
@@ -1,8 +1,9 @@
 import time
 
+
 class GroupHelper:
     def __init__(self, app):
-            self.app = app
+        self.app = app
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
@@ -12,54 +13,55 @@ class GroupHelper:
             wd.find_element_by_name(field_name).send_keys(text)
 
     def fill_group_form(self, group):
-            wd = self.app.wd
-            self.change_field_value("group_name", group.name)
-            self.change_field_value("group_header", group.header)
-            self.change_field_value("group_footer", group.footer)
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
 
     def create(self, group):
-            wd = self.app.wd
-            self.open_groups_page()
-            # init group creation
-            wd.find_element_by_name("new").click()
-            # fill group form
-            self.fill_group_form(group)
-            # submit group creation
-            wd.find_element_by_name("submit").click()
-            self.return_to_groups_page()
+        wd = self.app.wd
+        self.open_groups_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        # fill group form
+        self.fill_group_form(group)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
 
     def open_groups_page(self):
-            wd = self.app.wd
+        wd = self.app.wd
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("delete")) > 0):
             wd.find_element_by_link_text("groups").click()
 
     def return_to_groups_page(self):
-            wd = self.app.wd
-            wd.find_element_by_link_text("group page").click()
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
 
     def delete_first_group(self):
-            wd = self.app.wd
-            self.open_groups_page()
-            self.select_first_group()
-            # submit deletion
-            wd.find_element_by_name("delete").click()
-            self.return_to_groups_page()
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
 
     def modify_first_group(self, new_group_data):
-            wd = self.app.wd
-            self.open_groups_page()
-            self.select_first_group()
-            # time.sleep(100)
-            wd.find_element_by_name("edit").click()
-            self.fill_group_form(new_group_data)
-            # submit modification
-            wd.find_element_by_name("update").click()
-            self.return_to_groups_page()
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        # time.sleep(100)
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
+        # submit modification
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
 
     def select_first_group(self):
-            wd = self.app.wd
-            wd.find_element_by_name("selected[]").click()
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
 
     def count(self):
-            wd = self.app.wd
-            self.open_groups_page()
-            return len(wd.find_elements_by_name("selected[]"))
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
