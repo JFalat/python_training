ff1c530a6fa2454a5459f75429aed6bc28f15071
JFalat <joanna.falatowicz@gmail.com>
2022-06-08 22:18:21 +0200

    Homework 16 final

--- conftest.py
+++ conftest.py
@@ -8,12 +8,14 @@ def app(request):
     global fixture
     browser = request.config.getoption("--browser")
     base_url = request.config.getoption("--baseUrl")
+    username = request.config.getoption("--username")
+    password = request.config.getoption("--password")
     if fixture is None:
         fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
             fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+    fixture.session.ensure_login(username=username, password=password)
     return fixture
 
 
@@ -25,6 +27,8 @@ def stop(request):
     request.addfinalizer(fin)
     return fixture
 
-def pytest_adoptation(parser):
+def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseURl", action="store", default="http://localhost/addressbook")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook")
+    parser.addoption("--username", action="store", default="admin")
+    parser.addoption("--password", action="store", default="secret")
