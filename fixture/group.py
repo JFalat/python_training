88181ddee774e2e2f38b4fb1c22a55bd8417d2a8
JFalat <joanna.falatowicz@gmail.com>
2022-06-16 23:25:20 +0200

    Unit 7 question

--- fixture/group.py
+++ fixture/group.py
@@ -49,6 +49,14 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+        self.group_cache = None
 
     def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
@@ -73,6 +81,10 @@ class GroupHelper:
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_elements_by_css_selector("input[value='%s']" % id).click()
 
     def count(self):
         wd = self.app.wd
